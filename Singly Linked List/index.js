8893273d8aa3d40acfe2980a3e6c6539ba27db8b
krabuchi <shashu95@yahoo.com>
2023-03-05 17:48:37 +0530

    sll pop method

--- Singly Linked List/index.js
+++ Singly Linked List/index.js
@@ -3,16 +3,17 @@
 function Node(val) {
     this.value = val;
     this.next = null;
+    return this;
 }
 
-function SinglyLinkedList () {
+function SinglyLinkedList() {
     this.head = null;
     this.tail = null;
     this.length = 0;
 
     this.push = function (val) {
         var node = new Node(val);
-        if(this.head === null) {
+        if (this.head === null) {
             this.head = node;
             this.tail = node;
         } else {
@@ -21,5 +22,23 @@ function SinglyLinkedList () {
         }
         this.length++;
         return this;
+    }
+
+    this.pop = function () {
+        if (!this.head) return;
+        var pre = this.head;
+        var temp = pre;
+        while (temp) {
+            pre = temp;
+            temp = temp.next;
+        }
+        this.tail = pre;
+        pre.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
     }
 }
